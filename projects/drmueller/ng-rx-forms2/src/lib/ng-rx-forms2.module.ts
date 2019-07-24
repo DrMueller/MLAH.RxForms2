f329bef374937bebf5168b334002184de301b32a
Matthias Müller <matthias.mueller@trivadis.com>
2019-07-24 14:37:30 +0200

    WIP

--- projects/drmueller/ng-rx-forms2/src/lib/ng-rx-forms2.module.ts
+++ projects/drmueller/ng-rx-forms2/src/lib/ng-rx-forms2.module.ts
@@ -7,7 +7,7 @@ import { MatFormFieldModule, ErrorStateMatcher } from '@angular/material';
 import { RxErrorStateMatcherService } from './validation/services/rx-error-state-matcher.service';
 import { VALIDATION_ERROR_MAPPER_TOKEN } from './validation/services/error-mappers/constants';
 import { MinLengthErrorMapperService } from './validation/services/error-mappers/min-length-error-mapper.service';
-import { ValidationErrorsDirective } from './directives/validation-errors.directive';
+import { ValidationErrorsDirective } from './validation/directives/validation-errors.directive';
 
 @NgModule({
   imports: [
@@ -15,7 +15,7 @@ import { ValidationErrorsDirective } from './directives/validation-errors.direct
     MatFormFieldModule
   ],
   declarations: [ValidationErrorsComponent, ValidationErrorsDirective],
-  exports: [ValidationErrorsComponent]
+  exports: [ValidationErrorsComponent, ValidationErrorsDirective]
 })
 
 export class NgRxForms2Module {
